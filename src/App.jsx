d6cf4b9a59dd3ff5879bcf7ad9babb873df5595c
prpr <prpr@123.com>
2024-05-14 16:37:33 +0800

    .

--- src/App.jsx
+++ src/App.jsx
@@ -108,7 +108,7 @@ function CallView() {
         {call && participantInSpotlight && (
           <ParticipantView
             participant={participantInSpotlight}
-            // ParticipantViewUI={null}
+            ParticipantViewUI={ParticipantViewUI}
             VideoPlaceholder={VideoPlaceholder}
           />
         )}
